61b1856835aca15adfc55588db67c1bb7ffe3673
Dhanush S H <dhanushsh700@gmail.com>
2026-08-11 21:51:51 +0530

    Add Function Declaration

---
+++ 5.Functions/FunctionDeclaration.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void greet(); // Function Declaration
+
+void main()
+{
+    greet();
+    greet();
+}
+
+//Function definition
+void greet()
+{
+    printf("Hello, welcome to C programming\n");
+}
